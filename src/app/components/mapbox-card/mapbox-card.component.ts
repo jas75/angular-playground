9a4f7d268cedaa0b4c0aa723faf29ca3330a7025
EPITA\$COV000-LLGP7C05138I <jason.azoulay@epita.fr>
2023-04-13 18:02:24 +0200

    setup test

--- src/app/components/mapbox-card/mapbox-card.component.ts
+++ src/app/components/mapbox-card/mapbox-card.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import * as mapboxgl from 'mapbox-gl';
+import { environment } from 'src/environments/environment.test';
 
 @Component({
   selector: 'app-mapbox-card',
   templateUrl: './mapbox-card.component.html',
   styleUrls: ['./mapbox-card.component.scss']
 })
-export class MapboxCardComponent {
+export class MapboxCardComponent implements OnInit{
 
+  map?: mapboxgl.Map;
+  style = 'mapbox://styles/mapbox/streets-v12';
+  accessToken = environment.mapboxAccessToken;
+  lat = 26.3398;
+  lng = -81.7787;
+
+  ngOnInit(): void {
+    console.log(this.accessToken)
+    this.map = new mapboxgl.Map({
+      accessToken:
+       this.accessToken,
+      container: 'map',
+      style: this.style,
+      zoom: 2,
+      center: [this.lng, this.lat],
+    });
+
+    console.log(this.map)
+  }
 }
