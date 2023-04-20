51236013c105a6791d8529cdfa4b380f1d2a8455
EPITA\$COV000-LLGP7C05138I <jason.azoulay@epita.fr>
2023-04-20 18:01:11 +0200

    add different layers on ile de france

--- src/app/components/mapbox-card/mapbox-card.component.ts
+++ src/app/components/mapbox-card/mapbox-card.component.ts
@@ -28,21 +28,42 @@ export class MapboxCardComponent implements OnInit{
 
     this.map.on('load', () => {
 
-      this.map?.addSource('countries', {
+      // this.map?.addSource('countries', {
+      //   type: 'geojson',
+      //   data: 'https://www.data.gouv.fr/fr/datasets/r/00c0c560-3ad1-4a62-9a29-c34c98c3701e'
+      // });
+
+      this.map?.addSource('communes', {
         type: 'geojson',
-        data: 'https://raw.githubusercontent.com/nvkelso/natural-earth-vector/master/geojson/ne_110m_admin_0_countries.geojson'
+        data: 'https://raw.githubusercontent.com/gregoiredavid/france-geojson/master/regions/ile-de-france/communes-ile-de-france.geojson'
       });
+      
+      // this.map?.addSource('regions', {
+      //   type: 'geojson',
+      //   data: 'chemin/vers/le/fichier/regions.geojson'
+      // });
   
       this.map?.addLayer({
-        'id': 'countries',
+        'id': 'communes-layer',
         'type': 'fill',
-        'source': 'countries',
+        'source': 'communes',
         'layout': {},
         'paint': {
-          'fill-color': 'red',
-          'fill-opacity': 0.5
+          'fill-color': [
+            'match',
+            ['get', 'nom_comm'],
+            'Paris', 'red',
+            'Bobigny', '#0074D9',
+            'Montfermeil', '#2ECC40',
+            /* ajouter autant de communes que nécessaire */
+            /* 'nom_comm' est le nom de la propriété contenant le nom de la commune */
+            /* '#XXXXXX' est le code hexadécimal de la couleur souhaitée pour cette commune */
+            '#CCCCCC' /* couleur par défaut si le nom de la commune n'est pas trouvé */
+          ],
+          'fill-opacity': 0.8
         }
       });
+
     })
   }
 }
