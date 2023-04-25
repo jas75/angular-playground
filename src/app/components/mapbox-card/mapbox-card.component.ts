7a905fcf181220e69e3ef401ea328df6d5bbaef0
EPITA\$COV000-LLGP7C05138I <jason.azoulay@epita.fr>
2023-04-25 11:59:52 +0200

    remove layers on zoom and add switchable layers

--- src/app/components/mapbox-card/mapbox-card.component.ts
+++ src/app/components/mapbox-card/mapbox-card.component.ts
@@ -26,101 +26,87 @@ export class MapboxCardComponent implements OnInit{
     this.http.get('/assets/data/communes.geojson').subscribe(data => {
       this.data = data;
       console.log(this.accessToken)
-      this.map = new mapboxgl.Map({
-        accessToken:
-         this.accessToken,
-        container: 'map',
-        style: this.style,
-        zoom: this.zoom,
-        center: [this.lng, this.lat],
+      this.createMap();
+      this.map?.on('load', () => {
+          this.addRegionLayer();
       });
-  
-      this.map.on('load', () => {
-  
-        // this.map?.addSource('countries', {
-        //   type: 'geojson',
-        //   data: 'https://www.data.gouv.fr/fr/datasets/r/00c0c560-3ad1-4a62-9a29-c34c98c3701e'
-        // });
-  
-        // ts
-        
-        // this.map?.addSource('regions', {
-        //   type: 'geojson',
-        //   data: 'chemin/vers/le/fichier/regions.geojson'
-        // });
-    
-        
-          // this.map?.addLayer({
-          //   id: 'my-layer',
-          //   type: 'fill',
-          //   source: {
-          //     type: 'geojson',
-          //     data: this.data,
-          //   },
-          //   paint: {
-          //     'fill-color': 'transparent',
-          //     'fill-opacity': 0.5,
-          //   },
-          // });
-        
 
-          // this.map?.addLayer({
-          //   id: 'border-layer',
-          //   type: 'line',
-          //   source: {
-          //     type: 'geojson',
-          //     data: this.data
-          //   },
-          //   paint: {
-          //     'line-color': '#ffffff',
-          //     'line-width': 1
-          //   }
-          // });
+      this.map?.on('zoom', () => {
+        const zoom = this.map?.getZoom();
 
-          this.map?.addLayer({
-            id: 'region-layer',
-            type: 'fill',
-            source: {
-              type: 'geojson',
-              data: this.data
-            },
-            paint: {
-              'fill-color': [
-                'match',
-                ['get', 'region'],
-                'Occitanie', '#FF0000',
-                'Provence-Alpes-Côte d\'Azur', '#00FF00',
-                'Île-de-France', '#0000FF',
-                'Hauts-de-France', '#FFFF00',
-                'Nouvelle-Aquitaine', '#FF00FF',
-                'Auvergne-Rhône-Alpes', '#00FFFF',
-                'Bretagne', '#800080 ',
-                'Centre-Val de Loire', '#FFA500',
-                'Pays de la Loire', '#008000',
-                'Bourgogne-Franche-Comté', '#800000',
-                'Grand Est', '#000080',
-                'Normandie', '#008080',
-                /* Ajoutez d'autres couleurs pour chaque région */
-                '#000000' /* Couleur par défaut */
-              ],
-              "fill-opacity": 0.5
-            }
-          });
-
-          this.map?.addLayer({
-            id: 'border-layer',
-            type: 'line',
-            source: {
-              type: 'geojson',
-              data: this.data
-            },
-            paint: {
-              'line-color': '#ffffff',
-              'line-width': 0.2
-            }
-          });
-      });
+        if (zoom as number > 10) {
+          this.hideLayers();
+        } else {
+          this.displayRegionLayer();
+        }
+      })
 
     })
+  }
+
+  public createMap() {
+    this.map = new mapboxgl.Map({
+      accessToken:
+       this.accessToken,
+      container: 'map',
+      style: this.style,
+      zoom: this.zoom,
+      center: [this.lng, this.lat],
+    });
+  }
+
+  public addRegionLayer() {
+    this.map?.addLayer({
+      id: 'region-layer',
+      type: 'fill',
+      source: {
+        type: 'geojson',
+        data: this.data
+      },
+      paint: {
+        'fill-color': [
+          'match',
+          ['get', 'region'],
+          'Occitanie', '#FF0000',
+          'Provence-Alpes-Côte d\'Azur', '#00FF00',
+          'Île-de-France', '#0000FF',
+          'Hauts-de-France', '#FFFF00',
+          'Nouvelle-Aquitaine', '#FF00FF',
+          'Auvergne-Rhône-Alpes', '#00FFFF',
+          'Bretagne', '#800080 ',
+          'Centre-Val de Loire', '#FFA500',
+          'Pays de la Loire', '#008000',
+          'Bourgogne-Franche-Comté', '#800000',
+          'Grand Est', '#000080',
+          'Normandie', '#008080',
+          /* Ajoutez d'autres couleurs pour chaque région */
+          '#000000' /* Couleur par défaut */
+        ],
+        "fill-opacity": 0.5
+      }
+    });
+
+    this.map?.addLayer({
+      id: 'border-layer',
+      type: 'line',
+      source: {
+        type: 'geojson',
+        data: this.data
+      },
+      paint: {
+        'line-color': '#ffffff',
+        'line-width': 0.2
+      }
+    });
+  }
+
+
+  public displayRegionLayer() {
+    this.map?.setLayoutProperty('region-layer', 'visibility', 'visible');
+    this.map?.setLayoutProperty('border-layer', 'visibility', 'visible');
+  }
+  public hideLayers() {
+    this.map?.setLayoutProperty('region-layer', 'visibility', 'none'); // Cacher le layer
+    this.map?.setLayoutProperty('border-layer', 'visibility', 'none');
   }
 }
